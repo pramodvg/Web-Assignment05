f330511fce54d080528362d17ab3a4c40c4ab22f
pridetoracer <pridetoracer@gmail.com>
2021-07-26 21:11:27 +0530

    Hello World

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
+app.use((req,res)=>{
+    res.render("index");
+})
 
 app.listen(3000)
